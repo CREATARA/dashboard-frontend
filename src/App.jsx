417a3005fc1c397760c883bc3fb724ca1a979b9f
Codewithkrishna98 <mg305796@gmail.com>
2025-08-08 17:01:08 +0530

    removing clerk

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,26 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { SignedIn, SignedOut, SignIn, SignUp, UserButton } from "@clerk/clerk-react";
 import HomePage from './components/HomePage';
 import Ve4Dashboard from './components/Ve4Dashboard';
 import In40Dashboard from './components/In40Dashboard';
 import './App.css';
 
-// This component wraps any private content.
-// It will show the content if the user is signed in,
-// otherwise it will redirect them to the sign-in page.
-const PrivateRoute = ({ children }) => {
-  return (
-    <>
-      <SignedIn>{children}</SignedIn>
-      <SignedOut>
-        {/* With the provider setup, Clerk will handle this redirect automatically */}
-        <SignIn routing="path" path="/sign-in" />
-      </SignedOut>
-    </>
-  );
-};
-
 function App() {
   return (
     <>
-      <header className="absolute top-0 right-0 p-4 z-10">
-        <SignedIn>
-          <UserButton afterSignOutUrl="/sign-in" />
-        </SignedIn>
-      </header>
+      {/* The header with the user button has been removed. */}
       <Routes>
-        {/* Private Routes */}
-        <Route path="/" element={<PrivateRoute><HomePage /></PrivateRoute>} />
-        <Route path="/ve4" element={<PrivateRoute><Ve4Dashboard /></PrivateRoute>} />
-        <Route path="/in40" element={<PrivateRoute><In40Dashboard /></PrivateRoute>} />
-
-        {/* Public Sign-In and Sign-Up Routes */}
-        <Route
-          path="/sign-in/*"
-          element={
-            <div className="flex justify-center items-center min-h-screen">
-              <SignIn routing="path" path="/sign-in" />
-            </div>
-          }
-        />
-        <Route
-          path="/sign-up/*"
-          element={
-            <div className="flex justify-center items-center min-h-screen">
-              <SignUp routing="path" path="/sign-up" />
-            </div>
-          }
-        />
+        {/* All routes are now public. */}
+        <Route path="/" element={<HomePage />} />
+        <Route path="/ve4" element={<Ve4Dashboard />} />
+        <Route path="/in40" element={<In40Dashboard />} />
       </Routes>
     </>
   );
 }
 
 export default App;
+
 
 
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import { ClerkProvider } from '@clerk/clerk-react'
-import { BrowserRouter ,useNavigate} from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
-if (!PUBLISHABLE_KEY) {
-  throw new Error('Missing Publishable Key')
-}
-const ClerkProviderWithRoutes = () => {
-  const navigate = useNavigate();
-
-  return (
-    <ClerkProvider
-      publishableKey={PUBLISHABLE_KEY}
-      navigate={(to) => navigate(to)}
-    >
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
       <App />
-    </ClerkProvider>
-  );
-};
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-  <BrowserRouter>
-    <ClerkProviderWithRoutes />
-  </BrowserRouter>
-  </StrictMode>
-  
-)
+    </BrowserRouter>
+  </React.StrictMode>
+);
