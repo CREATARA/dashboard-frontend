779b6998641cfb270f898c4f610969cb85cabc5f
Codewithkrishna98 <mg305796@gmail.com>
2025-08-13 10:32:30 +0530

    in40 update

--- src/components/In40Dashboard.jsx
+++ src/components/In40Dashboard.jsx
@@ -291,7 +291,7 @@ const [data, setData] = useState(DUMMY_DATA);
             </div>
             <div className="w-[220px] h-[140px] rounded-3xl gap-2 bg-secondry flex flex-col justify-center p-4">
               <span className="text-xl">Kill Switch</span>
-              <span className="text-3xl">{data.kill ? "Active" : "Inactive/Off"}</span>
+              <span className="text-3xl">{data.kill ? "Active" : "Inactive"}</span>
             </div>
             <div className="w-[220px] h-[140px] rounded-3xl gap-2 bg-secondry flex flex-col justify-center p-4">
               <span className="text-xl">Push Button</span>
