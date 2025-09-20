1431b7c5951dfb1c1c4eece0a7c97c2d76f67016
CREATARA <akhil@creatara.com>
2025-09-20 17:10:02 +0530

    Update In40PowerChartModal.jsx

--- src/components/In40PowerChartModal.jsx
+++ src/components/In40PowerChartModal.jsx
@@ -158,7 +158,7 @@ const In40PowerChartModal = ({ isOpen, onClose }) => {
                                 <YAxis 
                                     label={{ value: 'Power (Watts)', angle: -90, position: 'insideLeft', fill: '#A0AEC0' }} 
                                     stroke="#facc15" 
-                                    domain={[1500]} 
+                                    domain={[1500, 'auto']} 
                                 />
                                 <Tooltip content={<CustomTooltip />} />
                                 <Legend />
